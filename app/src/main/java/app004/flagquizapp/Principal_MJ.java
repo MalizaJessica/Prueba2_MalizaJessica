74d9a9548ad4f041bc26e37c497223000da1fe6f
MalizaJessica <jessicamaliza19@gmail.com>
2022-01-26 19:49:06 -0500

    commit 19:48

---
+++ app/src/main/java/app004/flagquizapp/Principal_MJ.java
@@ -0,0 +1,14 @@
+package app004.flagquizapp;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+
+public class Principal_MJ extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_main);
+    }
+}
