c368134c2075340e350a6a3b13a39fc8b29014ac
MalizaJessica <jessicamaliza19@gmail.com>
2022-01-26 19:30:27 -0500

    commit 19:30

--- app/src/main/java/app004/flagquizapp/LoginActivity_MJ.java
+++ app/src/main/java/app004/flagquizapp/LoginActivity_MJ.java
@@ -1,5 +1,20 @@
 package app004.flagquizapp;
 
-public class LoginActivity_MJ {
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+import android.widget.EditText;
 
+public class LoginActivity_MJ extends AppCompatActivity {
+    EditText user;
+    EditText clave;
+    Button button_aceptar;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_main);
+        user = (EditText) findViewById(R.id.user);
+        clave = (EditText) findViewById(R.id.clave);
+        button_aceptar = (Button) findViewById(R.id.button_aceptar);
+    }
 }
